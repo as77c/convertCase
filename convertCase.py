0db153e6b8c66c9a56e9efc6a108b6f17c8159b1
Amin <xh6@hotmail.es>
2019-03-04 10:44:33 +0300

    first commit

---
+++ convertCase.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import argparse
+
+parser = argparse.ArgumentParser(description="This is tool will genrate all letter cases of a given string")
+parser.add_argument("-s", "--string", metavar="", required=True, help="specify a string")
+args = parser.parse_args()
+
+def letter_case(text):
+    words = []
+    text = text.lower()
+    meta_print("START")
+    for char in range(len(text)):
+        words = words +(helper(text, char + 1))
+    printLine(words)
+    meta_print("END")
+    
+def meta_print(status):
+    print "=" * 10, status, "=" * 10
+    
+def helper(text, step):
+    text = text.lower()
+    temp = list(text)
+    word = [];
+    for char in range(len(text)):
+        if(char + step <= len(text)):
+            temp[char : char + step] = [text[char : char + step].upper()]
+            word.append(''.join(temp))
+            temp = list(text)
+    return word
+            
+def printLine(words):
+    for word in words:
+        print word
+    print len(words), "cases are generated"
+    
+    
+
+
+letter_case(args.string)
